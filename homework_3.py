369d16d8a4fe0a392e440703d6dcf654e0673792
MatF <furgala.mat@gmail.com>
2023-11-18 23:26:27 +0100

    Adding homework files

--- homework_3.py
+++ homework_3.py
@@ -1,38 +1,56 @@
 # Dla oprocentowania 7%
 funds = 30_000
-capitalize = funds
-percentage = 1.075
-print("Symulacja dla oprocentowania 7%:")
-print("Środki początkowe:", funds, "Środki po pierwszej kapitalizacji:", round(capitalize * percentage, 2))
-capitalize *= percentage
-print("Środki po drugiej kapitalizacji:", round(capitalize * percentage, 2))
-capitalize *= percentage
-print("Środki po trzeciej kapitalizacji:", round(capitalize * percentage, 2))
-capitalize *= percentage
-print("Finalny zysk:", round(capitalize - funds, 2), "PLN")
+percentage = 7.5
+capitalize = round(percentage/4,2)
+print("Symulacja dla oprocentowania 7,5%:")
+print("Oprocentowanie kwartalne:", capitalize,"%.")
+print("Środki początkowe:", funds)
+print("Saldo po pierwszej kapitalizacji:", round(funds * (1 + capitalize / 100), 2))
+funds *= (1 + capitalize / 100)
+print("Saldo po drugiej kapitalizacji:", round(funds * (1 + capitalize/ 100), 2))
+funds *= (1 + capitalize / 100)
+print("Saldo po trzeciej kapitalizacji:", round(funds * (1 + capitalize / 100), 2))
+funds *= (1 + capitalize / 100)
+print("Saldo po czwartej kapitalizacji:", round(funds * (1 + capitalize / 100), 2))
+funds *= (1 + capitalize / 100)
+profit_1year = funds - 30_000
+print("Finalny zysk:", round(profit_1year, 2), "PLN")
+print("Finalny saldo:", round(profit_1year+30_000, 2), "PLN")
 print()
 # Dla oprocentowania 8%
 funds = 30_000
-capitalize = funds
-percentage = 1.080
-print("Symulacja dla oprocentowania 8%:")
-print("Środki początkowe:", funds, "Środki po pierwszej kapitalizacji:", round(capitalize * percentage, 2))
-capitalize *= percentage
-print("Środki po drugiej kapitalizacji:", round(capitalize * percentage, 2))
-capitalize *= percentage
-print("Środki po trzeciej kapitalizacji:", round(capitalize * percentage, 2))
-capitalize *= percentage
-print("Finalny zysk:", round(capitalize - funds, 2), "PLN")
+percentage = 8.0
+capitalize = round(percentage/4,2)
+print("Symulacja dla oprocentowania 7,5%:")
+print("Oprocentowanie kwartalne:", capitalize,"%.")
+print("Środki początkowe:", funds)
+print("Saldo po pierwszej kapitalizacji:", round(funds * (1 + capitalize / 100), 2))
+funds *= (1 + capitalize / 100)
+print("Saldo po drugiej kapitalizacji:", round(funds * (1 + capitalize/ 100), 2))
+funds *= (1 + capitalize / 100)
+print("Saldo po trzeciej kapitalizacji:", round(funds * (1 + capitalize / 100), 2))
+funds *= (1 + capitalize / 100)
+print("Saldo po czwartej kapitalizacji:", round(funds * (1 + capitalize / 100), 2))
+funds *= (1 + capitalize / 100)
+profit_1year = funds - 30_000
+print("Finalny zysk:", round(profit_1year, 2), "PLN")
+print("Finalny saldo:", round(profit_1year+30_000, 2), "PLN")
 print()
 # Dla oprocentowania 8.25
 funds = 30_000
-capitalize = funds
-percentage = 1.0825
-print("Symulacja dla oprocentowania 8.25%:")
-print("Środki początkowe:", funds, "Środki po pierwszej kapitalizacji:", round(capitalize * percentage, 2))
-capitalize *= percentage
-print("Środki po drugiej kapitalizacji:", round(capitalize * percentage, 2))
-capitalize *= percentage
-print("Środki po trzeciej kapitalizacji:", round(capitalize * percentage, 2))
-capitalize *= percentage
-print("Finalny zysk:", round(capitalize - funds, 2), "PLN")
+percentage = 8.25
+capitalize = round(percentage/4,2)
+print("Symulacja dla oprocentowania 7,5%:")
+print("Oprocentowanie kwartalne:", capitalize,"%.")
+print("Środki początkowe:", funds)
+print("Saldo po pierwszej kapitalizacji:", round(funds * (1 + capitalize / 100), 2))
+funds *= (1 + capitalize / 100)
+print("Saldo po drugiej kapitalizacji:", round(funds * (1 + capitalize/ 100), 2))
+funds *= (1 + capitalize / 100)
+print("Saldo po trzeciej kapitalizacji:", round(funds * (1 + capitalize / 100), 2))
+funds *= (1 + capitalize / 100)
+print("Saldo po czwartej kapitalizacji:", round(funds * (1 + capitalize / 100), 2))
+funds *= (1 + capitalize / 100)
+profit_1year = funds - 30_000
+print("Finalny zysk:", round(profit_1year, 2), "PLN")
+print("Finalny saldo:", round(profit_1year+30_000, 2), "PLN")
